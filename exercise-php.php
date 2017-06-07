c8891f0a418cbb0075ab156e282ad65e1c6475a4
mcdollano <mcdollano@gmail.com>
2017-06-07 16:54:36 -0700

    PHP Loop Function

---
+++ exercise-php.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+
+<?php
+
+	$names1 = array("paul","james","chris", "steph", "klay");
+	$names2 = array("dwayne", "james", "chris", "steph", "klay");
+
+	echo $names1[0],
+
+?>
+
+</body>
+</html>
